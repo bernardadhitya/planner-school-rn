71b242fa518fba1aebc92596b4f045092a7eab88
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-01-18 12:14:26 +0700

    [Bernard] Student to see uploaded image on submitted tab

--- src/Components/AssignmentPanel/MySubmissionCardContent.js
+++ src/Components/AssignmentPanel/MySubmissionCardContent.js
@@ -61,27 +61,23 @@ const MySubmissionCardContent = (props) => {
   );
 
   const renderSubmittedContent = () => (
-    <View>
-      <TouchableOpacity onPress={() => {}}>
-        <View style={{
-          marginTop: 12,
-        }}>
-          <Card>
-            <Layout style={styles.row}>
-              <Layout style={styles.column, {marginRight: 35, justifyContent: 'center'}}>
-                <Image
-                  source={{uri: image}}
-                  style={{height: 50, width: 50}}
-                />
-              </Layout>
-              <Layout style={styles.column, {justifyContent: 'center'}}>
-                <Text style={{fontFamily: 'Medium', fontSize: 10}}>{fileName}</Text>
-                <Text style={{fontFamily: 'Regular', fontSize: 8}}>2.4 MB</Text>
-              </Layout>
-            </Layout>
-          </Card>
-        </View>
-      </TouchableOpacity>
+    <View style={{
+      marginTop: 12,
+    }}>
+      <Card onPress={() => onClick()}>
+        <Layout style={styles.row}>
+          <Layout style={styles.column, {marginRight: 35, justifyContent: 'center'}}>
+            <Image
+              source={{uri: image}}
+              style={{height: 50, width: 50}}
+            />
+          </Layout>
+          <Layout style={styles.column, {justifyContent: 'center'}}>
+            <Text style={{fontFamily: 'Medium', fontSize: 10}}>{fileName}</Text>
+            <Text style={{fontFamily: 'Regular', fontSize: 8}}>2.4 MB</Text>
+          </Layout>
+        </Layout>
+      </Card>
     </View>
   )
 
